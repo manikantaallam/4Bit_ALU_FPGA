f764f489b64621d81c773cf11b9b441128d526c0
Venkata Mani Kanta Allam <37476329+manikantaallam@users.noreply.github.com>
2020-12-23 15:59:08 +0100

    Additional components

---
+++ Additional_Components/ROMs/DecimalEquivalentsROMBinDataGen.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+int BinaryCode(int n);
+
+int main()
+{
+
+//printf("library ieee;\n"   "use ieee.std_logic_1164.all;\n\n");
+
+for (int i=0; i<=255; i++) 
+{
+
+ int x = i;
+ 
+    while ( x > 0)
+    {   
+        int digit  = x % 10;
+        x = x/10;
+        printf("%d",  BinaryCode(digit)); 
+} 
+
+
+if (i == 0)
+{
+  printf(" \"11000000ZZZZZZZZZZZZZZZZ");  
+}
+
+
+if (i > 0 )
+  if (i <10 )
+  {
+{
+  printf("ZZZZZZZZZZZZZZZZ");  
+}}
+
+
+if (i > 9)
+{
+if (i < 100 )
+{
+  printf("ZZZZZZZZ");  
+}
+}
+printf("\" ,  "); 
+printf(" --%d ",i);
+printf("\n \""); 
+
+    
+}
+ return 0;
+}
+
+
+//function for binary value replacement
+
+int BinaryCode(int n)
+{
+//int n;
+int m = 0;
+
+  if (n == 0)
+{
+    m = 11000000; //dp,g,f,e,d,c,b,a    0
+} 
+else if (n == 1)
+{
+    m = 11111001;  //dp,g,f,e,d,c,b,a   1
+}
+else if (n == 2)
+{
+    m = 10100100;  //dp,g,f,e,d,c,b,a   2
+}
+else if (n == 3)
+{
+    m = 10110000;  //dp,g,f,e,d,c,b,a   3     
+}
+else if (n == 4)
+{
+    m = 10011001;  //dp,g,f,e,d,c,b,a   4   
+}
+else if (n == 5)   
+{
+    m = 10010010;  //dp,g,f,e,d,c,b,a    5
+}
+else if (n == 6)
+{
+    m = 10000010;  //dp,g,f,e,d,c,b,a    6
+}
+else if (n == 7)
+{
+    m = 11111000;  //dp,g,f,e,d,c,b,a    7
+}
+else if (n == 8)
+{
+    m = 10000000;  //dp,g,f,e,d,c,b,a    8
+}
+else if (n == 9)
+{
+    m = 10010000;  //dp,g,f,e,d,c,b,a    9
+}
+   return m;
+   
+}
---
+++ Additional_Components/ROMs/ROM_English_Equivalents.vhdl
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+--use ieee.numeric_std.all;
+
+
+entity ROM_English_Equivalents is
+  port (
+        --clk     :in std_logic;
+       -- reset   :in std_logic;
+
+        --Address for ROM Data
+        Cur_D_St_E_Val :in integer range 0 to 3;
+        
+
+        --Output for Seven Segments // Shall port map accordingly to the SSHandler component
+        CD_E_V_SS_out :out std_logic_vector(23 downto 0)  --dp,g,f,e,d,c,b,a --dp,g,f,e,d,c,b,a --dp,g,f,e,d,c,b,a        
+         
+  );
+end ROM_English_Equivalents;
+
+architecture struct of ROM_English_Equivalents is  
+
+
+  type ROM is Array (0 to 3) of std_logic_vector(23 downto 0);  
+  signal ROM_Data :ROM := (
+      
+    "1111100110100011ZZZZZZZZ" ,   --o1   (--1oZ)
+    "1010010010100011ZZZZZZZZ" ,   --o2   (--2oZ)
+    "110001101000110010100011" ,   --oPC  (--CPo)
+    "00100011ZZZZZZZZZZZZZZZZ"     --o.   (--o.ZZ)    
+
+  );
+
+begin
+
+  CD_E_V_SS_out <= ROM_Data (Cur_D_St_E_Val);
+
+end struct;
